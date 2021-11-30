1d857140c9ae2cf325b9590b49e6f96748b10c4b
sakib12345 <sakib35-1680@diu.edu.bd>
2021-11-30 18:08:41 +0600

    image component added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,24 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import ProfilePhoto from '../../public/images/propic.png'
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
       <h2>
-          <Link href='/'>
-              <a>Back to the Home Page</a>
-          </Link>
+        <Link href="/">
+          <a className="foo">
+            back to the home page
+          </a>
+        </Link>
       </h2>
+      <Image
+        src={ProfilePhoto} // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   );
 }
